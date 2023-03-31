ef2a7d571aafe332ed7d8bb8fe900c1619005063
Hazrat Ali <85784766+Atik-Hasan-8@users.noreply.github.com>
2023-03-31 16:00:11 +0600

    Time: 54 ms (95.92%), Space: 15.1 MB (56.51%) - LeetHub

---
+++ 0two-sum/0two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        container={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if diff in container:
+                return [i,container[diff]]
+            container[nums[i]]=i
+        return []
+        
+        
