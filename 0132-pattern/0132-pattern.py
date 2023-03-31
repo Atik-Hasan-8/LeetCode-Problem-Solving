76f78d72e106250f36bb55522ef7a56f3a4344a8
Hazrat Ali <85784766+Atik-Hasan-8@users.noreply.github.com>
2023-03-31 16:29:23 +0600

    Time: 380 ms (47.10%), Space: 32.2 MB (32.7%) - LeetHub

---
+++ 0132-pattern/0132-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack=[]
+        curmin=nums[0]
+        
+        for i in nums[1:]:
+            while stack and i>=stack[-1][0]:
+                stack.pop()
+            if stack and i>stack[-1][1]:
+                return True
+            stack.append([i,curmin])
+            curmin=min(curmin,i)
+        return False
+            
+            
+       
+        
