0d4c018c6cdcc8df8c033d5be356af1fb559de88
Hazrat Ali <85784766+Atik-Hasan-8@users.noreply.github.com>
2023-03-31 16:28:09 +0600

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- 0top-k-frequent-elements/0top-k-frequent-elements.py
+++ 0top-k-frequent-elements/0top-k-frequent-elements.py
@@ -1,18 +1,19 @@
 class Solution:
-     def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
-        # O(1) time 
-        if k == len(nums):
-            return nums
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        # 1. build hash map : character and how often it appears
-        # O(N) time
-        count = Counter(nums)   
-        # 2-3. build heap of top k frequent elements and
-        # convert it into an output array
-        # O(N log k) time
-        return heapq.nlargest(k, count.keys(), key=count.get) 
-        
-        
+        my_dict={}
+        arr=[[] for _ in range(len(nums)+1)]
+        for i in nums:
+            my_dict[i]=1+my_dict.get(i,0)
+        for n,c in my_dict.items():
+            arr[c].append(n)
+        res=[]
+        for i in range(len(arr)-1,0,-1):
+            for n in arr[i]:
+                res.append(n)
+                if len(res)==k:
+                    return res
+            
             
         
        
