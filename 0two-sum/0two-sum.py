c21a13bbb2375cc7e5cc30a86b547f71827f4341
Hazrat Ali <85784766+Atik-Hasan-8@users.noreply.github.com>
2023-03-31 16:01:43 +0600

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        data={}
-        for index,elements in enumerate(nums):
-            complement=target-elements
-            if complement in data:
-                return[data[complement],index]
-            data[elements]=index
+        for i in range(len(nums)):
             
+            for j in range(i+1,len(nums)):
+                
+                if nums[i]+nums[j]==target:
+                    
+                    return [i,j]
         return []
